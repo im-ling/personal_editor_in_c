25a097449b93bca0ca2a47146f4f211a785d9868
NowOrNever <tobeabetterman_dl@163.com>
2021-02-26 14:49:24 +0800

    feat: Entering raw mode: step 18, Error handling

--- kilo.c
+++ kilo.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -6,12 +7,21 @@
 
 struct termios orig_termios;
 
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        die("tcsetattr");
+    }
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        die("tcgetattr");
+    }
     atexit(disableRawMode);
     
     struct termios raw = orig_termios;
@@ -21,18 +31,24 @@ void enableRawMode() {
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN |ISIG);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 10; // 1 second
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        die("tcsetattr");
+    }
 }
 
 int main() {
     enableRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            die("read");
+        }
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         }else{
             printf("%d ('%c')\r\n", c, c);
         }
-    };
+        if (c == 'q') break;
+    }
 	return 0;
 }
