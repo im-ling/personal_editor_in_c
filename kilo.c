a0bac1ae5b24afc6c59bc8a1be0c7e2bc1cbd1be
NowOrNever <tobeabetterman_dl@163.com>
2021-02-26 14:52:54 +0800

    feat: Entering raw mode: step 19, Sections

--- kilo.c
+++ kilo.c
@@ -1,3 +1,4 @@
+/*** includes ***/
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,8 +6,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
 struct termios orig_termios;
 
+/*** terminal ***/
 void die(const char *s) {
     perror(s);
     exit(1);
@@ -36,6 +39,7 @@ void enableRawMode() {
     }
 }
 
+/*** init ***/
 int main() {
     enableRawMode();
     while (1) {
