6c25dae00b8f85db71784c26c1eb97c1e8491137
NowOrNever <tobeabetterman_dl@163.com>
2021-03-03 14:43:20 +0800

    feat: A text viewer: step 88, Tabs and the cursor: cx-to-rx

--- kilo.c
+++ kilo.c
@@ -171,6 +171,15 @@ int getWindowSize(int *rows, int *cols) {
 }
 
 /*** row operations ***/
+int editorRowCxToRx(erow *row, int cx) {
+    int rx = 0;
+    for (int j = 0; j < cx; j++) {
+        if (row->chars[j] == '\t')
+            rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+        rx++;
+    }
+    return rx;
+}
 
 void editorUpdateRow(erow *row){
     int tabs = 0;
