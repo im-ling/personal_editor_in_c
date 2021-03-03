1ad1b0c259c48c06defce7c73de31d67196fc3af
NowOrNever <tobeabetterman_dl@163.com>
2021-03-03 15:57:02 +0800

    feat: A text viewer: step 92, Status bar: status-bar-make-room

--- kilo.c
+++ kilo.c
@@ -301,10 +301,8 @@ void editorDrawRows(struct abuf *ab) {
             if (len > E.screencols) len = E.screencols;
             abAppend(ab, &E.row[filerow].render[E.coloff], len);
         }
-        abAppend(ab, "\x1b[K", 3);    
-        if (y < E.screenrows - 1) {
-            abAppend(ab, "\r\n", 2);
-        }
+        abAppend(ab, "\x1b[K", 3);
+        abAppend(ab, "\r\n", 2);
     }
 }
 
@@ -420,6 +418,7 @@ void initEditor() {
     E.numrows = 0;
     E.row = NULL;
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+    E.screenrows -= 1;
 }
 
 int main(int argc, char *argv[]) {
