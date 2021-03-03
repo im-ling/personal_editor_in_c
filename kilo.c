52aa44b11f97237888adb992686d56ffdadd38bc
NowOrNever <tobeabetterman_dl@163.com>
2021-03-03 16:00:46 +0800

    feat: A text viewer: step 94, Status bar: filename

--- kilo.c
+++ kilo.c
@@ -49,6 +49,7 @@ struct editorConfig {
     int screencols;
     int numrows;
     erow *row;
+    char *filename;
     struct termios orig_termios;
 };
 
@@ -219,6 +220,9 @@ void editorAppendRow(char *s, size_t len) {
 
 /*** file i/o ***/
 void editorOpen(char *filename) {
+    free(E.filename);
+    E.filename = strdup(filename);
+
     FILE *fp = fopen(filename, "r");
     if (!fp) die("fopen");
 
@@ -428,6 +432,8 @@ void initEditor() {
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.filename = NULL;
+
     if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
     E.screenrows -= 1;
 }
