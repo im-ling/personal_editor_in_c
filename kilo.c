2dadb467c4a53987178bcf0c2114eca9c90a88da
NowOrNever <tobeabetterman_dl@163.com>
2021-03-03 15:55:06 +0800

    feat: A text viewer: step 91, Move to the end of the line with End

--- kilo.c
+++ kilo.c
@@ -381,7 +381,8 @@ void editorProcessKeypress() {
             break;
 
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows)
+                E.cx = E.row[E.cy].size;
             break;
 
         case PAGE_UP:
@@ -393,7 +394,7 @@ void editorProcessKeypress() {
                     E.cy = E.rowoff + E.screenrows - 1;
                     if (E.cy > E.numrows) E.cy = E.numrows;
                 }
-                
+
                 int times = E.screenrows;
                 while (times--)
                     editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
