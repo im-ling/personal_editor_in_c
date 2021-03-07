43e66cb6e7ab2e052cd635b4db5060417fd34587
NowOrNever <tobeabetterman_dl@163.com>
2021-03-07 21:14:42 +0800

    feat: A text editor: step 129, Save as… :  abort-save

--- kilo.c
+++ kilo.c
@@ -356,7 +356,11 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as: %s");
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        if (E.filename == NULL) {
+            editorSetStatusMessage("Save aborted");
+            return;
+        }
     };
 
     int len;
@@ -522,7 +526,7 @@ char *editorPrompt(char *prompt) {
 
         int c = editorReadKey();
         if (c == '\x1b') {
-            editorSetStatusMessage("");
+            editorSetStatusMessage("123");
             free(buf);
             return NULL;
         } else if (c == '\r') {
