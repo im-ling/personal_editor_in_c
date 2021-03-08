146f7d095570febe90546c7e229deafb395d5111
NowOrNever <tobeabetterman_dl@163.com>
2021-03-08 17:31:55 +0800

    feat: Syntax highlighting: step 163, Detect filetype : detect-filetype

--- kilo.c
+++ kilo.c
@@ -446,6 +446,8 @@ void editorOpen(char *filename) {
     free(E.filename);
     E.filename = strdup(filename);
 
+    editorSelectSyntaxHighlight();
+
     FILE *fp = fopen(filename, "r");
     if (!fp) die("fopen");
 
@@ -469,6 +471,7 @@ void editorSave() {
             editorSetStatusMessage("Save aborted");
             return;
         }
+        editorSelectSyntaxHighlight();
     };
 
     int len;
