3f8c8043affb9f98e7a426bac88cba331a2bdd22
NowOrNever <tobeabetterman_dl@163.com>
2021-03-08 10:23:23 +0800

    fix: fix bug in step 132

--- kilo.c
+++ kilo.c
@@ -197,7 +197,8 @@ int editorRowCxToRx(erow *row, int cx) {
 
 int editorRowRxToCx(erow *row, int rx) {
     int cur_rx = 0;
-    for (int cx = 0; cx < row->size; cx++) {
+    int cx = 0;
+    for (cx = 0; cx < row->size; cx++) {
         if (row->chars[cx] == '\t')
             cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
         cur_rx++;
