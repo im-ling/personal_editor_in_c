135d448cee4a70d5b6bd7927b72d09bd5d921a21
NowOrNever <tobeabetterman_dl@163.com>
2021-03-08 11:54:15 +0800

    feat: A text editor: step 140, Search forward and backward : search-arrows

--- kilo.c
+++ kilo.c
@@ -414,11 +414,18 @@ void editorFindCallback(char *query, int key) {
         direction = 1;
     }
 
+    if (last_match == -1) direction = 1;
+    int current = last_match;
+
     for (int i = 0; i < E.numrows; i++) {
+        current += direction;
+        if (current == -1) current = E.numrows -1;
+        else if (current == E.numrows) current = 0;
         erow *row = &E.row[i];
         char *match = strstr(row->render, query);
         if (match) {
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
