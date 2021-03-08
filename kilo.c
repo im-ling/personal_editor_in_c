479daa6399a51e8f8f6b0a310073457f2045e5b6
NowOrNever <tobeabetterman_dl@163.com>
2021-03-08 16:21:35 +0800

    feat: Syntax highlighting: step 153, Colorful numbers : syntax-while

--- kilo.c
+++ kilo.c
@@ -197,10 +197,13 @@ void editorUpdateSyntax(erow *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
-    for (int i = 0; i < row->rsize; i++) {
-        if (isdigit(row->render[i])){
+    int i = 0;
+    while (i < row->rsize) {
+        char c = row->render[i];
+        if (isdigit(c)) {
             row->hl[i] = HL_NUMBER;
         }
+        i++;
     }
 }
 
